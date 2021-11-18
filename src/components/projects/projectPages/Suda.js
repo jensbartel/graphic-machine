acaf5108b5071038e4b4578162bd45f6dbbeeb73
jensbartel <jb3278@columbia.edu>
2021-11-18 21:51:23 +0100

    TH evening

--- src/components/projects/projectPages/Suda.js
+++ src/components/projects/projectPages/Suda.js
@@ -1,23 +1,19 @@
 import LargeTextCenter from '../reuseComponents/LargeTextCenter';
-// import LargeImageLeft from '../reuseComponents/LargeImageLeft';
-// import MoritaData from '../../../data/projectData/MoritaData';
-// import ThreeImages from '../reuseComponents/ThreeImages';
 import ThreeImagesGrid from '../reuseComponents/ThreeImagesGrid';
 import TwoImages from '../reuseComponents/TwoImages';
 
 import desktopImg from '../../../img/suda/suda_macbook.jpg';
-// import ipadImg from '../../../img/morita/morita_ipad.jpg';
 import iphoneImg from '../../../img/suda/suda_iphone.jpg';
 import iphoneImg2 from '../../../img/suda/suda_iphone-2.jpg';
 
 const SudaProject = () => {
-    // const { headline2, headline3, FootNoteText } = MoritaData;
-
     return (
         <>
             <ThreeImagesGrid img1={desktopImg} img2={iphoneImg} img3={iphoneImg2} />
             <LargeTextCenter headline='Introducing the Artist' />
             <TwoImages />
+            <LargeTextCenter headline='Bilingual: English & Japanese Pages' />
+
             {/* div below: replace later */}
             <div style={{ height: '4rem' }}></div>
         </>
--- src/components/projects/reuseComponents/TwoImages.js
+++ src/components/projects/reuseComponents/TwoImages.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import imgLeft from '../../../img/suda/suda-chronology.jpg';
+import imgRight from '../../../img/suda/suda-exhibitions.jpg';
 
 const TwoImages = () => {
-    return <div>Hello World</div>;
+    return (
+        <div className='two-images'>
+            <div className='left-img'>
+                <img src={imgLeft} alt='' />
+            </div>
+            <div className='right-img'>
+                <img src={imgRight} alt='' />
+            </div>
+        </div>
+    );
 };
 
 export default TwoImages;
