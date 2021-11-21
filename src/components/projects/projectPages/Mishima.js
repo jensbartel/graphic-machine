ea9aee6c3c6ccc13683a368f997fda6b1641d389
jensbartel <jb3278@columbia.edu>
2021-11-21 20:29:48 +0100

    Sunday evening

--- src/components/projects/projectPages/Mishima.js
+++ src/components/projects/projectPages/Mishima.js
@@ -1,7 +1,11 @@
-import OneImage from '../reuseComponents/OneImage';
-import mishima1 from '../../../img/mishima/mishima_macbook.jpg';
+// import OneImage from '../reuseComponents/OneImage';
+import ThreeImages from '../reuseComponents/ThreeImages';
+import mishima1 from '../../../img/mishima/mishima-macbook.jpg';
+import mishima2 from '../../../img/mishima/mishima-ipad-eng.jpg';
+import mishima3 from '../../../img/mishima/mishima-iphone.jpg';
 import { webPages } from '../../../data/projectOverviewData/WebPageObjects';
 import ProjectTitle from '../reuseComponents/ProjectTitle';
+import LargeTextCenter from '../reuseComponents/LargeTextCenter';
 
 const Mishima = () => {
     const { projects } = webPages;
@@ -10,8 +14,10 @@ const Mishima = () => {
     return (
         <div>
             <ProjectTitle project={project} />
-            <OneImage img1={mishima1} cssStyle='one-image-variant' />
-            <div style={{ marginTop: '4rem' }}></div>
+            {/* <OneImage img1={mishima1} cssStyle='one-image-variant' /> */}
+            <ThreeImages img1={mishima1} img2={mishima2} img3={mishima3} />
+            <LargeTextCenter headline='Layout & Sales Value' />
+            {/* <div style={{ marginTop: '4rem' }}></div> */}
 
             {/* div below: replace later */}
             <div style={{ height: '4rem' }}></div>
