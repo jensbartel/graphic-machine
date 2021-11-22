838c5595ed3890b7bd2edcd2058407777900fec2
jensbartel <jb3278@columbia.edu>
2021-11-22 15:47:58 +0100

    Mishima project page mostly done

--- src/components/projects/projectPages/Mishima.js
+++ src/components/projects/projectPages/Mishima.js
@@ -1,11 +1,17 @@
 // import OneImage from '../reuseComponents/OneImage';
 import ThreeImages from '../reuseComponents/ThreeImages';
+import TwoImages from '../reuseComponents/TwoImages';
+import OneImage from '../reuseComponents/OneImage';
 import mishima1 from '../../../img/mishima/mishima-macbook.jpg';
 import mishima2 from '../../../img/mishima/mishima-ipad-eng.jpg';
 import mishima3 from '../../../img/mishima/mishima-iphone.jpg';
+import mishima4 from '../../../img/mishima/mishima-layout-01.jpg';
+import mishima5 from '../../../img/mishima/mishima-layout-rows-1.jpg';
+import mishima6 from '../../../img/mishima/parallax-visuals1.jpg';
 import { webPages } from '../../../data/projectOverviewData/WebPageObjects';
 import ProjectTitle from '../reuseComponents/ProjectTitle';
 import LargeTextCenter from '../reuseComponents/LargeTextCenter';
+import mishimaData from '../../../data/projectData/MishimaData';
 
 const Mishima = () => {
     const { projects } = webPages;
@@ -17,6 +23,11 @@ const Mishima = () => {
             {/* <OneImage img1={mishima1} cssStyle='one-image-variant' /> */}
             <ThreeImages img1={mishima1} img2={mishima2} img3={mishima3} />
             <LargeTextCenter headline='Layout & Sales Value' />
+            <TwoImages imgLeft={mishima4} imgRight={mishima5} />
+            <LargeTextCenter headline='Parallax scroll' />
+            {/* <TwoImages imgLeft={mishima6} imgRight={mishima5} /> */}
+            <OneImage img1={mishima6} cssStyle='one-image' />
+            <LargeTextCenter headline='Mishima Ritsue thing' text={mishimaData} />
             {/* <div style={{ marginTop: '4rem' }}></div> */}
 
             {/* div below: replace later */}
---
+++ src/data/projectData/MishimaData.js
@@ -0,0 +1,9 @@
+const mishimaData = `Various efforts were taken to negotiate the hideous amount of text, like separating it into sections and making chapters accessible by quick links. Footnotes can be previewed by hovering over them. 
+
+“VIP pages” that target art collectors are accessed by adding “/vip” to the URL. These pages are not linked from the main page for the sake of gallery discretion; URL was directly mailed to prospective clients. Media queries allow for desktop, large desktop, various tablet types and mobile phone.<br><br>
+
+<span style="display: block; color: #8f8f8f; font-size: 0.7rem; line-height: 0.5 !important">Exhibition on view in January–February 2021 in Kyoto and Tokyo, Fall–Winter 2021: Paris.<br>
+Site online since December 2019. Tools: React, SASS, Git/Github. <br>Hosting: Google Cloud Platform. Installation view photo credits: H. Ichikawa.</span>
+`;
+
+export default mishimaData;
