f6d7f50fea2ea9228ad00172acbf43b8fabc0177
jensbartel <jb3278@columbia.edu>
2021-11-30 17:25:22 +0100

    ClientApp page updates

--- src/components/projects/projectPages/ClientApp.js
+++ src/components/projects/projectPages/ClientApp.js
@@ -5,10 +5,12 @@ import ProjectTitle from '../reuseComponents/ProjectTitle';
 import TwoImages from '../reuseComponents/TwoImages';
 
 import backoffice1 from '../../../img/clientapp/clientapp-backoffice-1.jpg';
+import systemArchitecture from '../../../img/clientapp/clientApp-visual-01.png';
 import iOS1 from '../../../img/clientapp/clientapp-1.jpg';
 import iOS2 from '../../../img/clientapp/clientapp-2.jpg';
 
 import { webApps } from '../../../data/projectOverviewData/WebAppObjects';
+import Headline from '../reuseComponents/Headline';
 
 const ClientApp = () => {
     useEffect(() => {
@@ -20,10 +22,16 @@ const ClientApp = () => {
     return (
         <>
             <ProjectTitle project={project} />
-            <TwoImages imgLeft={iOS1} imgRight={iOS2} />
+            <TwoImages imgLeft={iOS1} captionLeft={capLeft} imgRight={iOS2} captionRight={capRight} />
             <OneImage img1={backoffice1} cssStyle='one-image-variant-2' />
+            <Headline headline='System Overview' />
+            <OneImage img1={systemArchitecture} cssStyle='one-image-variant img-border' />
         </>
     );
 };
 
 export default ClientApp;
+
+// refasctor later
+const capLeft = `Sign up view for clients on iPad.`;
+const capRight = `Staff view to add notes.`;
--- src/components/projects/reuseComponents/TwoImages.js
+++ src/components/projects/reuseComponents/TwoImages.js
@@ -1,12 +1,14 @@
 const TwoImages = props => {
-    const { imgLeft, imgRight } = props;
+    const { imgLeft, imgRight, captionLeft, captionRight } = props;
     return (
         <div className='two-images'>
             <div className='left-img'>
                 <img src={imgLeft} alt='' />
+                {captionLeft && <p className='caption-no-margin'>{captionLeft}</p>}
             </div>
             <div className='right-img'>
                 <img src={imgRight} alt='' />
+                {captionLeft && <p className='caption-no-margin'>{captionRight}</p>}
             </div>
         </div>
     );
