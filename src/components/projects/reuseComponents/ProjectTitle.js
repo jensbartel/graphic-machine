d7822c12d68b5d04f8e5b61a091cbc4b8cce795c
jensbartel <jb3278@columbia.edu>
2021-12-01 12:48:53 +0100

    iphone further fixes

--- src/components/projects/reuseComponents/ProjectTitle.js
+++ src/components/projects/reuseComponents/ProjectTitle.js
@@ -21,15 +21,19 @@ const ProjectTitle = props => {
 
     return (
         <div className='project-title'>
-            <p>{titleChoice}</p>
-            <a href={livePage} target='_blank' rel='noreferrer'>
-                <i className='fas fa-map-marker-alt'></i>
-                <span className=''>URL</span>
-            </a>
-            <a href={github} target='_blank' rel='noreferrer'>
-                <i className='fab fa-github'></i>
-                <span className=''>Github</span>
-            </a>
+            <div className='title-and-links'>
+                <p>{titleChoice}</p>
+                <div>
+                    <a href={livePage} target='_blank' rel='noreferrer'>
+                        <i className='fas fa-map-marker-alt'></i>
+                        <span className=''>URL</span>
+                    </a>
+                    <a href={github} target='_blank' rel='noreferrer'>
+                        <i className='fab fa-github'></i>
+                        <span className=''>Github</span>
+                    </a>
+                </div>
+            </div>
             <p className='project-links'>
                 {descriptionChoice},&nbsp;{dateChoice}
             </p>
