c43d0e0056590f55a441d9291a01b264e1a95efe
jensbartel <jb3278@columbia.edu>
2021-12-02 19:06:15 +0100

    MMAS update 3

--- src/components/projects/projectPages/MMAS.js
+++ src/components/projects/projectPages/MMAS.js
@@ -10,28 +10,29 @@ import miya2 from '../../../img/mmas/miyanomori-outside-02.jpg';
 import miya3 from '../../../img/mmas/mmas-mainview-2.jpg';
 import miya4 from '../../../img/mmas/mmas-mainview-3.jpg';
 import miya5 from '../../../img/mmas/mmas-mainview-4.jpg';
+import miya6 from '../../../img/mmas/mmas-mainview-1.jpg';
 
 import { webApps } from '../../../data/projectOverviewData/WebAppObjects';
 import MMASData from '../../../data/projectData/MMASData';
+import LargeTextCenter from '../reuseComponents/LargeTextCenter';
 
 const MMAS = () => {
     const { projects } = webApps;
     const project = projects.MMAS;
-    const { mainView, searchMask, assistantEditor } = MMASData;
+    const { exterior1, exterior2, mainView, searchMask, assistantEditor, searchResults, searchWorkView, headline1, text1 } = MMASData;
 
     useEffect(() => {
-        document.title = 'MMAS';
+        document.title = 'Grifold Studio | MMAS';
     }, []);
 
     return (
         <>
             <ProjectTitle project={project} />
-            <TwoImages imgLeft={miya1} imgRight={miya2} />
+            <TwoImages imgLeft={miya1} captionLeft={exterior1} imgRight={miya2} captionRight={exterior2} />
             <OneImage img1={miya3} cssStyle='one-image mato' caption={html(mainView)} />
-            <TwoImages imgLeft={miya4} imgRight={miya5} captionLeft={searchMask} captionRight={assistantEditor} />
-
-            {/* below: remove later */}
-            <div style={{ height: '20rem' }}></div>
+            <TwoImages imgLeft={miya4} imgRight={miya5} captionLeft={searchMask} captionRight={html(assistantEditor)} />
+            <TwoImages imgLeft={miya6} imgRight={miya3} captionLeft={searchWorkView} captionRight={searchWorkView} />
+            <LargeTextCenter headline={headline1} text={text1} />
         </>
     );
 };
--- src/data/projectData/MMASData.js
+++ src/data/projectData/MMASData.js
@@ -1,9 +1,27 @@
-const mainView = `Left: Search Results. Right: Work View. Disclaimer: Images are from Google search and don't reflect the actual MMAS collection. All artwork data is fictional.<br><br><br>`; // note: the <br>s are a hack; remove
+const exterior1 = `Exterior view: Entrance.`;
 
-const searchMask = `Search mask.`;
+const exterior2 = `Exterior view: Garden.`;
 
-const assistantEditor = `Assistant Editor view.`;
+const mainView = `Left: Search Results. Right: Work View. Disclaimer: Images are from Google search and don't reflect the actual MMAS collection. All artwork data is fictional.
+<br><br>`;
 
-const MMASData = { mainView, searchMask, assistantEditor };
+const searchMask = `Search mask. Sidebar expanded.`;
+
+const assistantEditor = `Assistant Editor view. Sidebar collapsed.
+<br><br>`;
+
+const searchResults = `Search results.`;
+
+const searchWorkView = `Search view (left) and work view (right).`;
+
+const headline1 = `Miyanomori Museum of Art`;
+
+const text1 = `The Miyanomori Museum of Art is a private museun in <strong>Sapporo</strong> (Hokkaido) specializing in twentieth century and contemporary Japanese art, currently undergoing extensive renovation. Re-opening was planned for 2020 but delayed due to Covid. Collection holdings: about 5000 items, and growing. <br><br>
+
+As of 2020, <strong>no proper collections database existed</strong>; collection items were catalogued as individual pdf files. There is apparent need for a professional collections management solution, as the museum continues to grow. Design emphasis: Simplicity, straightforwardness, and a modern, easy-to-use interface. Note this project didn't proceed beyond concept stage as the museum insisted on the developer moving to Tokyo, which was not practical at the time.
+
+<br><br><br><br>`;
+
+const MMASData = { exterior1, exterior2, mainView, searchMask, assistantEditor, searchResults, searchWorkView, headline1, text1 };
 
 export default MMASData;
