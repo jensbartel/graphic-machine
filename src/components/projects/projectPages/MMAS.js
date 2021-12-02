79daa7d1033193f3f7c0665d52d5ea9f38ab68ad
jensbartel <jb3278@columbia.edu>
2021-12-02 17:41:18 +0100

    MMAS update 2

--- src/components/projects/projectPages/MMAS.js
+++ src/components/projects/projectPages/MMAS.js
@@ -1,16 +1,23 @@
 import { useEffect } from 'react';
+import html from 'react-html-parser';
 
+import OneImage from '../reuseComponents/OneImage';
 import ProjectTitle from '../reuseComponents/ProjectTitle';
 import TwoImages from '../reuseComponents/TwoImages';
 
 import miya1 from '../../../img/mmas/miyanomori-outside-01.jpg';
 import miya2 from '../../../img/mmas/miyanomori-outside-02.jpg';
+import miya3 from '../../../img/mmas/mmas-mainview-2.jpg';
+import miya4 from '../../../img/mmas/mmas-mainview-3.jpg';
+import miya5 from '../../../img/mmas/mmas-mainview-4.jpg';
 
 import { webApps } from '../../../data/projectOverviewData/WebAppObjects';
+import MMASData from '../../../data/projectData/MMASData';
 
 const MMAS = () => {
     const { projects } = webApps;
     const project = projects.MMAS;
+    const { mainView, searchMask, assistantEditor } = MMASData;
 
     useEffect(() => {
         document.title = 'MMAS';
@@ -20,6 +27,8 @@ const MMAS = () => {
         <>
             <ProjectTitle project={project} />
             <TwoImages imgLeft={miya1} imgRight={miya2} />
+            <OneImage img1={miya3} cssStyle='one-image mato' caption={html(mainView)} />
+            <TwoImages imgLeft={miya4} imgRight={miya5} captionLeft={searchMask} captionRight={assistantEditor} />
 
             {/* below: remove later */}
             <div style={{ height: '20rem' }}></div>
---
+++ src/data/projectData/MMASData.js
@@ -0,0 +1,9 @@
+const mainView = `Left: Search Results. Right: Work View. Disclaimer: Images are from Google search and don't reflect the actual MMAS collection. All artwork data is fictional.<br><br><br>`; // note: the <br>s are a hack; remove
+
+const searchMask = `Search mask.`;
+
+const assistantEditor = `Assistant Editor view.`;
+
+const MMASData = { mainView, searchMask, assistantEditor };
+
+export default MMASData;
